5483752a09fe2dc375eec55f61ede2d775d7d4b4
Kausik N <nkausik1999@gmail.com>
2021-10-05 17:04:16 +0530

    added random 2D and 3D point generation

--- Algorithms/RandomGenerator.py
+++ Algorithms/RandomGenerator.py
@@ -3,9 +3,12 @@ PlotGIFLibrary is a library for generation, editing and viewing of GIFs / Videos
 '''
 
 # Imports
+import cv2
 import random
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+from matplotlib.figure import Figure
 
 from .Libraries import PlotAnimateLibrary as PAL
 
@@ -14,11 +17,14 @@ YData = {}
 XData = {}
 plotData = {}
 
+# Main Vars
+fig = plt.figure()
+canvas = FigureCanvasAgg(fig)
+
 # Main Functions
+# OLD Functions
 # Random Generator Vis - Visualise the distribution of random number generator in python by visualising the frequency distribution
-
-
-def RandomGenerator_Vis(numRange=(0, 100), frameLim=(0, 100), nframes=100, show=True):
+def RandomGenerator_Vis_OLD(numRange=(0, 100), frameLim=(0, 100), nframes=100, show=True):
     global XData
     global YData
 
@@ -64,6 +70,101 @@ def RandomGenerator_PlotUpdate(i):
         plotData['ax'].set_ylim(YData['lim'][0], YData['lim'][1])
     plotData['plotVar'][newVal_Index].set_height(YData['data'][newVal_Index])
 
+# New Functions
+# Random Functions
+def RandomFrequencyDistribution_Vis(numRange=(0, 100), nframes=100, title=''):
+    # Setup Plot
+    global fig, canvas
+    fig = plt.figure()
+    canvas = FigureCanvasAgg(fig)
+
+    # Generate Random Numbers
+    randVals = np.random.randint(numRange[0], numRange[1]+1, nframes)
+    xs = list(range(numRange[0], numRange[1]+1))
+    curFreqs = np.zeros(len(xs))
+
+    Is = []
+    for r in randVals:
+        fig.clear()
+        plt.title(title)
+        ax = plt.axes()
+
+        curFreqs[xs.index(r)] += 1
+        # Plot the data
+        plt.bar(xs, curFreqs)
+
+        # Retrieve Image
+        canvas.draw()
+        buf = canvas.buffer_rgba()
+        I_plot = cv2.cvtColor(np.asarray(buf), cv2.COLOR_RGBA2RGB)
+        Is.append(I_plot)
+
+    plt.close(fig)
+    return Is
+
+def Random2DPointsGenerator_Vis(valuesBound=[(0, 100), (0, 100)], nframes=100, title=''):
+    # Setup Plot
+    global fig, canvas
+    fig = plt.figure()
+    canvas = FigureCanvasAgg(fig)
+
+    # Generate Random Points
+    randPointsX = np.random.uniform(valuesBound[0][0], valuesBound[0][1], nframes)
+    randPointsY = np.random.uniform(valuesBound[1][0], valuesBound[1][1], nframes)
+    randPoints = np.dstack((randPointsX, randPointsY))[0]
+
+    Is = []
+    for i in range(randPoints.shape[0]):
+        fig.clear()
+        plt.title(title)
+        ax = plt.axes()
+        ax.set_xlim(valuesBound[0][0], valuesBound[0][1])
+        ax.set_ylim(valuesBound[1][0], valuesBound[1][1])
+
+        # Plot the data
+        ax.scatter(randPoints[:i+1][:, 0], randPoints[:i+1][:, 1])
+
+        # Retrieve Image
+        canvas.draw()
+        buf = canvas.buffer_rgba()
+        I_plot = cv2.cvtColor(np.asarray(buf), cv2.COLOR_RGBA2RGB)
+        Is.append(I_plot)
+
+    plt.close(fig)
+    return Is
+
+def Random3DPointsGenerator_Vis(valuesBound=[(0, 100), (0, 100), (0, 100)], nframes=100, title=''):
+    # Setup Plot
+    global fig, canvas
+    fig = plt.figure()
+    canvas = FigureCanvasAgg(fig)
+
+    # Generate Random Points
+    randPointsX = np.random.uniform(valuesBound[0][0], valuesBound[0][1], nframes)
+    randPointsY = np.random.uniform(valuesBound[1][0], valuesBound[1][1], nframes)
+    randPointsZ = np.random.uniform(valuesBound[2][0], valuesBound[2][1], nframes)
+    randPoints = np.dstack((randPointsX, randPointsY, randPointsZ))[0]
+
+    Is = []
+    for i in range(randPoints.shape[0]):
+        fig.clear()
+        plt.title(title)
+        ax = plt.axes(projection='3d')
+        ax.set_xlim(valuesBound[0][0], valuesBound[0][1])
+        ax.set_ylim(valuesBound[1][0], valuesBound[1][1])
+
+        # Plot the data
+        ax.scatter3D(randPoints[:i+1][:, 0], randPoints[:i+1][:, 1], randPoints[:i+1][:, 2])
+
+        # Retrieve Image
+        canvas.draw()
+        buf = canvas.buffer_rgba()
+        I_plot = cv2.cvtColor(np.asarray(buf), cv2.COLOR_RGBA2RGB)
+        Is.append(I_plot)
+
+    plt.close(fig)
+    return Is
+    
 
 # Driver Code
 # # Params
--- StreamLitGUI/apps/apps_RandomGenerator.py
+++ StreamLitGUI/apps/apps_RandomGenerator.py
@@ -5,12 +5,13 @@ Stream lit GUI for Random Generator
 # Imports
 # import os
 # import cv2
-# import streamlit as st
+import streamlit as st
 # import json
 # import subprocess
 # import functools
 
 from Algorithms import RandomGenerator
+from Algorithms.Libraries import VideoUtils
 
 #############################################################################################################################
 # Repo Based Vars
@@ -26,7 +27,64 @@ from Algorithms import RandomGenerator
 
 
 # UI Functions
+def UI_RandomFrequencyDistribution():
+    USERINPUT_numRange = st.slider("Select Random Value Range", 1, 100, (1, 5), 1)
+    USERINPUT_nframes = st.number_input("Select Number of Values to Generate", 1, 500, 10, 1)
+    USERINPUT_saveFPS = USERINPUT_nframes / DEFAULT_VIDEO_DURATION
 
+    if st.button("Generate"):
+        # Process Inputs
+        Is = RandomGenerator.RandomFrequencyDistribution_Vis(USERINPUT_numRange, USERINPUT_nframes, 'Random Frequency Distribution')
+
+        # Display Outputs
+        st.markdown("## Generated Random Frequency Distribution")
+        VideoUtils.SaveFrames2Video(Is, DEFAULT_SAVEPATH_VIDEO, USERINPUT_saveFPS)
+        FixSavedVideoFile()
+        # Display Animation Video
+        st.video(DEFAULT_SAVEPATH_VIDEO_CONVERTED)
+
+def UI_Random2DPoints():
+    col1, col2 = st.columns(2)
+    USERINPUT_pointXBounds = col1.slider("Select X Values Bound", 1, 100, (1, 5), 1)
+    USERINPUT_pointYBounds = col2.slider("Select Y Values Bound", 1, 100, (1, 5), 1)
+    USERINPUT_nframes = st.number_input("Select Number of Points to Generate", 1, 500, 10, 1)
+    USERINPUT_saveFPS = USERINPUT_nframes / DEFAULT_VIDEO_DURATION
+
+    if st.button("Generate"):
+        # Process Inputs
+        Is = RandomGenerator.Random2DPointsGenerator_Vis([USERINPUT_pointXBounds, USERINPUT_pointYBounds], USERINPUT_nframes, 'Random 2D Points')
+
+        # Display Outputs
+        st.markdown("## Generated Random 2D Points")
+        VideoUtils.SaveFrames2Video(Is, DEFAULT_SAVEPATH_VIDEO, USERINPUT_saveFPS)
+        FixSavedVideoFile()
+        # Display Animation Video
+        st.video(DEFAULT_SAVEPATH_VIDEO_CONVERTED)
+
+def UI_Random3DPoints():
+    col1, col2, col3 = st.columns(3)
+    USERINPUT_pointXBounds = col1.slider("Select X Values Bound", 1, 100, (1, 5), 1)
+    USERINPUT_pointYBounds = col2.slider("Select Y Values Bound", 1, 100, (1, 5), 1)
+    USERINPUT_pointZBounds = col3.slider("Select Z Values Bound", 1, 100, (1, 5), 1)
+    USERINPUT_nframes = st.number_input("Select Number of Points to Generate", 1, 500, 10, 1)
+    USERINPUT_saveFPS = USERINPUT_nframes / DEFAULT_VIDEO_DURATION
+
+    if st.button("Generate"):
+        # Process Inputs
+        Is = RandomGenerator.Random3DPointsGenerator_Vis([USERINPUT_pointXBounds, USERINPUT_pointYBounds, USERINPUT_pointZBounds], USERINPUT_nframes, 'Random 3D Points')
+
+        # Display Outputs
+        st.markdown("## Generated Random 3D Points")
+        VideoUtils.SaveFrames2Video(Is, DEFAULT_SAVEPATH_VIDEO, USERINPUT_saveFPS)
+        FixSavedVideoFile()
+        # Display Animation Video
+        st.video(DEFAULT_SAVEPATH_VIDEO_CONVERTED)
+
+RANDOMGENERATOR_VISUALISAITON_MAP = {
+    "Random Frequency Distribution": UI_RandomFrequencyDistribution,
+    "Random 2D Points": UI_Random2DPoints,
+    "Random 3D Points": UI_Random3DPoints
+}
 
 # Repo Based Functions
 def random_generators():
@@ -34,19 +92,10 @@ def random_generators():
     st.header("Random Generators")
 
     # Load Inputs
-    USERINPUT_numRange = st.slider("Select Random Value Range", 1, 100, (1, 5), 1)
-    USERINPUT_nframes = st.number_input("Select Number of Values to Generate", 1, 500, 10, 1)
-    USERINPUT_frameLim = (0, 1)
-    USERINPUT_saveFPS = USERINPUT_nframes / st.number_input("Select Animation Duration", 0.1, 2.5, 1.0, 0.1)
+    USERINPUT_VisChoice = st.selectbox("Select Visualization Type", list(RANDOMGENERATOR_VISUALISAITON_MAP.keys()))
 
-    if st.button("Generate"):
-        # Process Inputs
-        animation = RandomGenerator.RandomGenerator_Vis(USERINPUT_numRange, USERINPUT_frameLim, USERINPUT_nframes, False)
-
-        # Display Outputs
-        st.markdown("## Generated Random Distribution")
-        RandomGenerator.PAL.SavePlotGIF(animation, DEFAULT_SAVEPATH_GIF, USERINPUT_saveFPS)
-        st.image(DEFAULT_SAVEPATH_GIF, "Generated Random Numbers", use_column_width=True)
+    # Process Inputs
+    RANDOMGENERATOR_VISUALISAITON_MAP[USERINPUT_VisChoice]()
     
 #############################################################################################################################
 # Driver Code
